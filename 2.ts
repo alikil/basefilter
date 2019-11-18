2fe91aea415fd3301df36eec38eaaabf17e135c8
Serhii Kalachnikov <alikil@ukr.net>
2019-11-18 09:15:18 +0200

    lint

--- 2.ts
+++ 2.ts
@@ -1,37 +1,36 @@
 import * as fs from "fs";
 import * as iconvlite from "iconv-lite";
 function readFileSync_encoding(filename: string | number | Buffer | import("url").URL, encoding: string) {
-    var content = fs.readFileSync(filename);
+    const content = fs.readFileSync(filename);
     return iconvlite.decode(content, encoding);
 }
 
-let md5 = ""
-let cap = ""
-let Solved = new Set()
+let md5 = "";
+let cap = "";
+const Solved = new Set();
 
-readFileSync_encoding("good.txt","win1251")
+readFileSync_encoding("good.txt", "win1251")
 .replace(/\[|\]/gm, "")
 .replace(/\},/gm, "}\n")
 .replace(/}\s?\n{/gm, "}|{")
 .split("|")
-.map((elem)=>{
+.map((elem) => {
     Solved.add({
         md5 : elem.match(/"md5":"(.*?)"/)[1].toString().trim(),
-        cap : elem.match(/"cap":"(.*?)"/)[1].toString().trim()        
-    })
-})
+        cap : elem.match(/"cap":"(.*?)"/)[1].toString().trim(),
+    });
+});
 
-let towrite = JSON.stringify([...Solved])
-readFileSync_encoding("badSolveFromList.txt","win1251")
+let towrite = JSON.stringify([...Solved]);
+readFileSync_encoding("badSolveFromList.txt", "win1251")
 .replace(/}\s?\n{/gm, "}|{")
 .split("|")
-.map((elem)=>{
-    md5 = elem.match(/"md5":"(.*?)"/)[1].toString().trim()
-    cap = elem.match(/"cap":"(.*?)"/)[1].toString().trim()
-    console.log(elem)
-    const sSearch = `{"md5":"${md5}","cap":"${cap}"}`
-    var r = new RegExp(sSearch, 'g');
-    towrite = towrite.replace(r,'').replace(/\,\,/gm, ",");
-})
+.map((elem) => {
+    md5 = elem.match(/"md5":"(.*?)"/)[1].toString().trim();
+    cap = elem.match(/"cap":"(.*?)"/)[1].toString().trim();
+    const sSearch = `{"md5":"${md5}","cap":"${cap}"}`;
+    const r = new RegExp(sSearch, "g");
+    towrite = towrite.replace(r, "").replace(/\,\,/gm, ",");
+});
 
 fs.writeFileSync("new.txt", towrite);
